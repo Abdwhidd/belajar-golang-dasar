a85811494d492f582204bec0272cfc8b6308cf1e
Wahid <abdulwahidkaharr@gmail.com>
2023-10-08 22:28:12 +0700

    add string.go

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Abdul"
+	lastName := "Abdul Wahid"
+
+	fmt.Println(len(firstName)) // resultnya adalah 5
+	fmt.Println(lastName[0])    // resultnya 65 karena bytenya "A" adalah 69
+	fmt.Println(lastName[9])    // resultnya 105 karena bytenya "d" adalah 105
+	fmt.Println("Wahid"[1])     // resultnya 97 karena bytenya "w" adalah 97
+}
