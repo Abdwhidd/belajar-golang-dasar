684b44891397b4beca4f35f9a0f0d925b3a029eb
Wahid <abdulwahidkaharr@gmail.com>
2023-10-09 00:16:16 +0700

    string

--- string.go
+++ string.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println(lastName[0])    // resultnya 65 karena bytenya "A" adalah 69
 	fmt.Println(lastName[9])    // resultnya 105 karena bytenya "d" adalah 105
 	fmt.Println("Wahid"[1])     // resultnya 97 karena bytenya "w" adalah 97
+
+	fmt.Printf("halo %s", firstName) // resultnya halo Abdul%
 }
