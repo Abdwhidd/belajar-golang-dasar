97176656e8a8ae668aad376bcab6d1dbc1276abe
Wahid <abdulwahidkaharr@gmail.com>
2023-10-09 00:15:59 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Constant WAJIB langsung dideklarasikan valueanya
+	const (
+		firtsName = "Wahid"
+		lastName  = "Kahar"
+	)
+
+	fmt.Println(firtsName)
+	fmt.Println(lastName)
+}
